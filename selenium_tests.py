bb5508f848743581c74000c24333f45305556f11
zwang2244 <zwang2244@wisc.edu>
2021-11-01 14:18:33 -0500

    Update selenium_tests.py
    Sign up with duplicate username
    Sign up with random username

--- selenium_tests.py
+++ selenium_tests.py
@@ -9,13 +9,18 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import unittest;
+import string
+import random
 class TestLoginduplicate(unittest.TestCase):
   def setUp(self):
-    self.driver = webdriver.Chrome("drivers/chromedriver")
+    chrome_options = webdriver.ChromeOptions()
+    prefs = {"profile.default_content_setting_values.notifications" : 2}
+    chrome_options.add_experimental_option("prefs",prefs)
+    self.driver = webdriver.Chrome("drivers/chromedriver",chrome_options=chrome_options)
     self.vars = {}
   
-  def teardown_method(self):
-    self.driver.quit()
+  def tearDown(self):
+    self.driver.close()
   
   def test_loginduplicate(self):
     self.driver.get("http://localhost:3001/activations")
@@ -40,9 +45,34 @@ class TestLoginduplicate(unittest.TestCase):
     self.driver.find_element(By.XPATH, "//div[@id=\'content\']/div/div/div[2]/div/div/div/div/div/div[4]/div/div[3]/div/form/div/div[2]/div/input").click()
     self.driver.find_element(By.XPATH, "//div[@id=\'content\']/div/div/div[2]/div/div/div/div/div/div[4]/div/div[3]/div/form/div/div[2]/div/input").send_keys("password")
     self.driver.find_element(By.XPATH, "//div[2]/div/div/button/div/div/span").click()
-    WebDriverWait(self.driver, NaN).until(expected_conditions.presence_of_element_located((By.XPATH, "//form/div/div/div/div[3]")))
+    WebDriverWait(self.driver, 1000).until(expected_conditions.presence_of_element_located((By.XPATH, "//form/div/div/div/div[3]")))
     elements = self.driver.find_elements(By.XPATH, "//form/div/div/div/div[3]")
     assert len(elements) > 0
+    assert self.driver.find_element(By.XPATH,"//form/div/div/div/div[3]").text == "User with this e-mail address has been already registered"
+
+  def test_signup(self, email = None):
+    self.driver.get("http://localhost:3001/activations")
+    self.driver.set_window_size(1600, 900)
+    self.driver.find_element(By.CSS_SELECTOR, ".AppBar__menu-item").click()
+    self.driver.find_element(By.CSS_SELECTOR, ".LoginDialog__title--clickable").click()
+    self.driver.find_element(By.CSS_SELECTOR, ".Tabs > div > button:nth-child(1) > div").click()
+    self.driver.find_element(By.XPATH, "//div[@id=\'content\']/div/div/div[2]/div/div/div/div/div/div[4]/div/div[3]/div/form/div/div/div/input").click()
+    letters = string.ascii_letters
+    if(email == None):
+      email = ''.join(random.choice(letters) for i in range(10)) + "@gmail.com"
+    self.driver.find_element(By.XPATH, "//div[@id=\'content\']/div/div/div[2]/div/div/div/div/div/div[4]/div/div[3]/div/form/div/div/div/input").send_keys(email)
+    self.driver.find_element(By.XPATH, "//div[@id=\'content\']/div/div/div[2]/div/div/div/div/div/div[4]/div/div[3]/div/form/div/div[2]/div/input").click()
+    self.driver.find_element(By.XPATH, "//div[@id=\'content\']/div/div/div[2]/div/div/div/div/div/div[4]/div/div[3]/div/form/div/div[2]/div/input").send_keys("passworod")
+    self.driver.find_element(By.XPATH, "//div[@id=\'content\']/div/div/div[2]/div/div/div/div/div/div[4]/div/div[3]/div/form/div[2]/div/div/button/div/div/span").click()
+    self.driver.find_element(By.XPATH, "//div[@id=\'content\']/div/div/div[2]/div/div/div/div/div/div[4]/div/div[3]/div/form").click()
+    WebDriverWait(self.driver, 1000).until(expected_conditions.visibility_of_element_located((By.XPATH, "//div[@id=\'content\']/div/div/div[2]/div/div/div/div/div/div[4]/div/div[3]/div/form/div[2]")))
+    elements = self.driver.find_elements(By.XPATH, "//div[@id=\'content\']/div/div/div[2]/div/div/div/div/div/div[4]/div/div[3]/div/form/div[2]")
+    assert len(elements) > 0
+    assert self.driver.find_element(By.XPATH, "//div[@id=\'content\']/div/div/div[2]/div/div/div/div/div/div[4]/div/div[3]/div/form/div[2]").text == "The confirmation letter was sent to your e-mail"
+  
+
+
+
   
 if __name__ == '__main__':
   unittest.main()
